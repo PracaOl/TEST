e3b57fe99e111babf3439ff5c2f0f9e0c2c651fb
3109362 <3109362@TE-GDA-D-D12C16>
2022-03-04 13:30:24 +0100

    Dodaj pliki projektów.

---
+++ ProjektNowy/Program.cs
@@ -0,0 +1,13 @@
+﻿//deklarujemy zmienne na atrybuty funkcji
+double a, b, c;
+//deklarujemy zmienną na wejście od użytkownika
+
+//polecenie
+Console.WriteLine("Podaj parametry według wzoru ogólnego: F(x) = ax^2 + bx + c");
+//wypytaj o parametry, pytaj do skutku jeśli nie podano liczby
+    Console.WriteLine("Podaj a:");
+a = double.Parse(a);
+    Console.WriteLine("Podaj b:");
+b = double.Parse(b);
+    Console.WriteLine("Podaj c:");
+c = double.Parse(c);
